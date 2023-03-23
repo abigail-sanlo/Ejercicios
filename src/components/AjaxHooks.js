2e3d798c627b77581afdf588860c1fc8da8dbecd
abigailsan <abisl9160@gmail.com>
2023-03-23 17:55:52 -0600

    .

--- src/components/AjaxHooks.js
+++ src/components/AjaxHooks.js
@@ -12,13 +12,35 @@ export default function AjaxHooks() {
         )
     }
 
+    // useEffect(() => {
+    //     let url = 'https://pokeapi.co/api/v2/pokemon/';
+    //     fetch(url)
+    //     .then (res=>res.json())
+    //     .then((json) => {
+    //         json.results.forEach((el) => {
+    //             fetch(el.url).then((res) =>res.json()).then((json) =>{
+    //                 // console.log(json);
+    //                 let pokemon ={
+    //                     id: json.id,
+    //                     name:json.name,
+    //                     avatar: json.sprites.front_default
+    //                 };
+    //                 setPokemons((pokemons) =>[...pokemons,pokemon])
+
+    //         })
+    //         })
+    //     })
+    // }, []);
+
     useEffect(() => {
-        let url = 'https://pokeapi.co/api/v2/pokemon/';
-        fetch(url)
-        .then (res=>res.json())
-        .then((json) => {
-            json.results.forEach((el) => {
-                fetch(el.url).then((res) =>res.json()).then((json) =>{
+        //Nota: no es recomendable hacer async el useEffect porque es un antipatrón
+         const getPokemons = async (url)=>{
+            let res = await fetch(url),
+            json = await res.json
+            json.results.forEach(async (el) => {
+                let res = await fetch(el.url),
+                json= await res.json();
+
                     // console.log(json);
                     let pokemon ={
                         id: json.id,
@@ -27,9 +49,18 @@ export default function AjaxHooks() {
                     };
                     setPokemons((pokemons) =>[...pokemons,pokemon])
 
+            
             })
-            })
-        })
+        }  //-------->esta es la manera correcta de usar async await en un useEffect
+        getPokemons('https://pokeapi.co/api/v2/pokemon/')
+
+        
+        // let url = 'https://pokeapi.co/api/v2/pokemon/';
+        // fetch(url)
+        // .then (res=>res.json())
+        // .then((json) => {
+            
+        // })
     }, []);
     
     return(
